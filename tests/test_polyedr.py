069769d7142dd3e82be524dde4c8cd286cfe4fab
timurab27 <tim.abdulin2006@yandex.ru>
2025-06-20 17:45:03 +0300

    Update test_polyedr.py

--- tests/test_polyedr.py
+++ tests/test_polyedr.py
@@ -4,7 +4,7 @@ from unittest.mock import patch, mock_open
 from shadow.polyedr import Polyedr
 
 
-class TestPolyedr(unittest.TestCase):
+class TestPolyedr1(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -36,3 +36,41 @@ class TestPolyedr(unittest.TestCase):
 
     def test_num_edges(self):
         self.assertEqual(len(self.polyedr.edges), 16)
+
+class TestPolyedr2(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """100.0	45.0	45.0	30.0
+8	6	24
+-1.5	-1.5	1.5
+-1.5	1.5	1.5
+2.5	2.5	1.5
+1.5	-1.5	1.5
+-1.5	-1.5	-1.5
+-1.5	1.5	-1.5
+1.5	1.5	-1.5
+1.5	-1.5	-1.5
+4	1    2    3    4
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5
+4	8    7    6    5"""
+        fake_file_path = 'data/test.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 6)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 24)
+
+    def test_sum(self):
+        self.assertAlmostEqual(self.polyedr.sum, 18.0)
